2088b7e7d7448cc27c7dbfff93452873788c596c
Ingmar Jager <info@ingmarjager.nl>
2014-10-13 12:31:14 +0200

    Progress on database tool : - sift features computed and stored - colorhist features computed and stored

--- dbt.py
+++ dbt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import argparse
 import feature_extraction as ft
 import glob
@@ -8,8 +9,20 @@ import image_search
 import pickle
 import os.path
 
-def compute_features(image_list, output, feature_function):
-    fname = output + '.pkl'
+def feature_active(name):
+    return (args.feature == name or args.feature == 'all')
+
+
+def load_features(name):
+    fname = args.prefix + base + '_' + name + '.pkl'
+    feat = None
+    with open(fname, 'rb') as f:
+        print 'Loading', fname
+        feat = pickle.load(f)
+    return feat 
+
+def compute_features(image_list, name, feature_function):
+    fname = args.prefix + base + '_' + name + '.pkl'
     if os.path.isfile(fname):
         compute = raw_input("Found existing features: " + fname + " Do you want to recompute them? ([Y]/N): ")
     else:
@@ -19,6 +32,9 @@ def compute_features(image_list, output, feature_function):
         with open(fname, 'wb') as f:
             print 'saving features to', fname , '...'
             pickle.dump(features, f)
+    else:
+        # load features
+        features = load_features(name)
     return features
 
 
@@ -50,41 +66,115 @@ for type_ in types:
 # Get file name without extension and prefix
 base=os.path.basename(args.database).split('.')[0]
 
-# sift features
-
 sift_features = None
-if args.feature == 'sift' or args.feature == 'all':
-    compute_features(image_list, args.prefix + base + '_sift', ft.get_sift_features)
+sift_vocabulary = None
+harris_features = None
+colorhist_features = None
+meta_features = None
+
+# sift features
+if feature_active('sift'):
+    sift_features = compute_features(image_list, 'sift', ft.get_sift_features)
 
     # visual vocabulary
     
-    fname = args.prefix + base + '_vocabulary.pkl'
+    fname = args.prefix + base + '_sift_vocabulary.pkl'
     if os.path.isfile(fname):
         compute = raw_input("Found existing vocabulary: " + fname + " Do you want to recompute it? ([Y]/N): ")
     else:
         compute = 'Y'
     if compute == 'Y' or compute == '':
-        if sift_features == None:
-            fname = args.prefix + base +  '_sift.pkl'
-            if os.path.isfile(fname):
-                print 'loading sift features'
-                with open(fname, 'rb') as f:
-                    sift_features = pickle.load(f)
-            else:
-                print 'Error, please generate sift features first.'
 	print 'Creating vocabulary ... \n'
-        voc = Vocabulary.Vocabulary(base)
-        voc.train(sift_features, args.clusters)
-        fname = args.prefix + base + '_vocabulary.pkl'
+        sift_vocabulary = Vocabulary.Vocabulary(base)
+        sift_vocabulary.train(sift_features, args.clusters)
+        fname = args.prefix + base + '_sift_vocabulary.pkl'
 	with open(fname, 'wb') as f: 
-            pickle.dump(voc,f)
+            pickle.dump(sift_vocabulary,f)
 
 
-if args.feature == 'colorhist' or args.feature == 'all':
-    compute_features(image_list, args.prefix + base + '_colorhist', ft.get_colorhist)
+if feature_active('colorhist'):
+    colorhist_features = compute_features(image_list, 'colorhist', ft.get_colorhist)
 
-if args.feature == 'harris' or args.feature == 'all':
-    compute_features(image_list, args.prefix + base + '_harris', ft.get_harris_features)
+if feature_active('harris'):
+    harris_features = compute_features(image_list, 'harris', ft.get_harris_features)
 
-if args.feature == 'meta' or args.feature == 'all':
-    compute_features(image_list, args.prefix + base + '_meta', ft.get_meta_data)
+#if feature_active('meta'):
+#    meta_features = compute_features(image_list, 'meta', ft.get_meta_data)
+
+
+
+print 'Creating database ... \n'
+
+db_name = args.prefix + base + '.db'
+
+#check if database already exists
+new = False
+if os.path.isfile(db_name):
+    action = raw_input('Database already exists. Do you want to (r)emove, (a)ppend or (q)uit? ')
+    print 'action =', action
+else:
+    action = 'c'
+
+if action == 'r':
+    print 'removing database', db_name , '...'
+    os.remove(db_name)
+    new = True
+
+elif action == 'a':
+    print 'appending to database ... '
+
+elif action == 'c':
+    print 'creating database', db_name, '...'
+    new = True
+
+else:
+    print 'Quit database tool'
+    sys.exit(0)
+
+
+
+# create indexer
+indx = image_search.Indexer(db_name) 
+if new == True:
+    indx.create_tables()
+
+
+# Loading necessary features if not in memory yet. Then add features 
+# to their corresponding database tables.
+if feature_active('sift'): 
+    if sift_vocabulary == None:
+        sift_vocabulary = load_features('sift_vocabulary')
+    if sift_features == None:
+        sift_features = load_features('sift')
+
+    print 'Adding sift features to database ... '
+    for i in range(len(image_list)):
+        indx.add_to_index('sift', image_list[i], sift_features[image_list[i]], sift_vocabulary)
+
+if feature_active('colorhist'):
+    if colorhist_features == None:
+        colorhist_features = load_features('colorhist')
+
+    print 'Adding colorhist features to database ... '
+    for i in range(len(image_list)):
+        indx.add_to_colorhist_index(image_list[i], colorhist_features[image_list[i]])
+
+if feature_active('harris'):
+    if harris_vocabulary == None:
+        harris_vocabulary = load_features('harris_vocabulary')
+    if harris_features == None:
+        harris_features = load_features('harris')
+
+    print 'Adding harris features to database ... '
+    for i in range(len(image_list)):
+        indx.add_to_index('harris', image_list[i], harris_features[image_list[i]], harris_vocabulary)
+
+#if feature_active('meta'):
+#    if meta_features == None:
+#        meta_features = load_features('meta')
+
+	
+# commit to database
+indx.db_commit()
+
+print 'Done\n'
--- image_search.py
+++ image_search.py
@@ -5,9 +5,8 @@ from pysqlite2 import dbapi2 as sqlite
 
 class Indexer(object):
 
-	def __init__(self, db, voc):
+	def __init__(self, db):
 		self.con = sqlite.connect(db)
-		self.voc = voc
 
 	def __del__(self):
 		self.con.close()
@@ -17,44 +16,53 @@ class Indexer(object):
 	
 	def create_tables(self):
 		self.con.execute('create table imlist(filename)') 
-		self.con.execute('create table imwords(imid,wordid,vocname)') 
-		self.con.execute('create table imhistograms(imid,histogram,vocname)') 
-		self.con.execute('create table colorhists(imid, hist)')
 		self.con.execute('create index im_idx on imlist(filename)') 
-		self.con.execute('create index wordid_idx on imwords(wordid)') 
-		self.con.execute('create index imid_idx on imwords(imid)') 
-		self.con.execute('create index imidhist_idx on imhistograms(imid)') 
+
+		self.con.execute('create table sift_imwords(imid,wordid,vocname)') 
+                self.con.execute('create index sift_imid_idx on sift_imwords(imid)') 
+		self.con.execute('create index sift_wordid_idx on sift_imwords(wordid)') 
+
+		self.con.execute('create table sift_imhistograms(imid,histogram,vocname)') 
+		self.con.execute('create index sift_imidhist_idx on sift_imhistograms(imid)') 
+
+		self.con.execute('create table colorhists(imid, hist)')
+		self.con.execute('create index colorhist_idx on colorhists(imid)') 
+
+		self.con.execute('create table harris_imwords(imid,wordid,vocname)') 
+                self.con.execute('create index harris_imid_idx on harris_imwords(imid)') 
+		self.con.execute('create index harris_wordid_idx on harris_imwords(wordid)') 
+
+		self.con.execute('create table harris_imhistograms(imid,histogram,vocname)') 
+		self.con.execute('create index harris_imidhist_idx on harris_imhistograms(imid)') 
+
+
 		self.db_commit()
 
-	def add_to_index(self, imname, descr):
+	def add_to_index(self, type, imname, descr, voc):
 		""" Take an image with feature descriptors, 
 			project on vocabulary and add to database. """
 
-		if self.is_indexed(imname): 
-			return
 		print 'indexing', imname
 
 		# get the imid
 		imid = self.get_id(imname)
 
 		#get the words
-		imwords = self.voc.project(descr)
+		imwords = voc.project(descr)
 		nbr_words = imwords.shape[0]
 
 		# link each word to image
 		for i in range(nbr_words):
 			word = imwords[i]
 			# wordid is the word number itself
-			self.con.execute("insert into imwords(imid,wordid,vocname) values (?,?,?)", (imid,word,self.voc.name))
+			self.con.execute("insert into "+type+"_imwords(imid,wordid,vocname) values (?,?,?)", (imid,word,voc.name))
 
 		# store word histogram for image
 		# use pickle to encode NumPy arrays as strings
-		self.con.execute("insert into imhistograms(imid,histogram,vocname) values (?,?,?)", (imid,pickle.dumps(imwords), self.voc.name))
+		self.con.execute("insert into "+type+"_imhistograms(imid,histogram,vocname) values (?,?,?)", (imid,pickle.dumps(imwords), voc.name))
 
-	def add_colorhist_to_index(self, imname, hist):
+	def add_to_colorhist_index(self, imname, hist):
 
-	#	if self.is_indexed(imname):
-	#		return
 		print 'indexing', imname
 
 		# get the imid
