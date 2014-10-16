54c268089085a98893952893fb947b3627950e09
Ingmar Jager <info@ingmarjager.nl>
2014-10-16 11:01:37 +0200

    Image search and queries for sift features works for query tool

--- image_search.py
+++ image_search.py
@@ -10,11 +10,11 @@ class Searcher:
 	def __del__(self):
 		self.con.close()
 
-	def candidates_from_word(self, imword):
+	def candidates_from_word(self, type, imword):
 		""" Get list of images containing imword/ """
 
 		im_ids = self.con.execute(
-				"select distinct imid from imwords where wordid=%d" % imword).fetchall()
+				"select distinct imid from "+type+"_imwords where wordid=%d" % imword).fetchall()
 		return [i[0] for i in im_ids]
 
 
@@ -31,7 +31,7 @@ class Searcher:
 		i = np.argsort(result)
 		return np.array(names)[i]
 
-	def candidates_from_histogram(self, imwords):
+	def candidates_from_histogram(self, type, imwords):
 		""" Get list of images wth similar words. """
 
 		# get the word ids
@@ -39,7 +39,7 @@ class Searcher:
 		# find candidates
 		candidates = []
 		for word in words:
-			c = self.candidates_from_word(word)
+			c = self.candidates_from_word(type, word)
 			candidates += c
 		
 		# take all unique words and reverse sort on occurence
@@ -62,22 +62,22 @@ class Searcher:
 
 
 	
-	def get_imhistogram(self, imname):
+	def get_imhistogram(self, type, imname):
 		""" Return the word histogram for an image. """
 
 		im_id = self.con.execute(
 				"select rowid from imlist where filename='%s'" % imname).fetchone()
 		s = self.con.execute(
-				"select histogram from imhistograms where rowid='%d'" % im_id).fetchone()
+				"select histogram from "+type+"_imhistograms where rowid='%d'" % im_id).fetchone()
 
 		# use pickle to decode NumPy arrays from string
 		return pickle.loads(str(s[0]))
 
-	def query(self, imname):
+	def query(self, type, imname):
 		""" Find a list of matching images for imname"""
 
 		h = self.get_imhistogram(imname)
-		candidates = self.candidates_from_histogram(h)
+		candidates = self.candidates_from_histogram(type,h)
 
 		matchscores = []
 		for imid in candidates:
@@ -91,6 +91,24 @@ class Searcher:
 			#return a sorted list of distances and databse ids
 			matchscores.sort()
 		return matchscores
+
+	def query_iw(self,type, h):
+            """ Find a list of matching images for image histogram h"""
+            candidates = self.candidates_from_histogram(type,h)
+
+            matchscores = []
+            for imid in candidates:
+                    # get the name
+                    cand_name = self.con.execute(
+                                    "select filename from imlist where rowid=%d" % imid).fetchone()
+                    cand_h = self.get_imhistogram(type, cand_name)
+                    cand_dist = np.sqrt( np.sum( (h-cand_h)**2 ) ) #use L2 distance
+                    matchscores.append( (cand_dist, imid) )
+
+                    #return a sorted list of distances and databse ids
+                    matchscores.sort()
+            return matchscores
+
 
 
 	def get_filename(self,imid):
---
+++ query.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import argparse
+import pickle
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import feature_extraction as ft
+import sys
+import image_search
+import os.path
+
+# Command line parsing is handled by the ArgumentParser object
+
+features = ['sift', 'colorhist', 'harris', 'meta', 'all']
+
+parser = argparse.ArgumentParser(description="Query tool to query the database created by the database tool (dbt.py). Retrieve images based on image content and metadata.")
+parser.add_argument("database", help="Path to the database to execute the query on.")
+parser.add_argument("query", help="Query image")
+parser.add_argument("feature", help="The type of feature to get results on. Chose from "+str(features))
+parser.add_argument("--prefix","-p",  help="prefix path to database directory, default = 'db/'", default="db/")
+
+args = parser.parse_args()
+
+# Get file name without extension and prefix
+base=os.path.basename(args.database).split('.')[0]
+
+
+def feature_active(name):
+    """ Check if feature 'name' is active
+    
+    i.e. the feature has been selected via a command line option to be used for processing"""
+    return (args.feature == name or args.feature == 'all')
+
+
+
+
+# Starting point of the script
+# =======================================
+
+if __name__ == '__main__':
+   
+    print '\nMulti Media Analysis Query Tool'
+    print '================================\n'
+    print "Query the database with [", args.query, "] for [", args.feature, "] features..."
+    db_name = args.database
+    search = image_search.Searcher(db_name)
+    if feature_active('sift'):
+        print 'Loading SIFT vocabulary ...'
+        fname = args.prefix + base + '_sift_vocabulary.pkl'
+        with open(fname, 'rb') as f:
+            sift_vocabulary = pickle.load(f)
+
+        sift_query = ft.get_sift_features([args.query])[args.query]
+        image_words = sift_vocabulary.project(sift_query)
+        print sift_query
+        print image_words
+        print 'Query database with a histogram...'
+        print search.candidates_from_histogram('sift', image_words)[:10]
+        candidates = search.query_iw('sift', image_words)
+        print candidates[0:10]
+        for cand in candidates[0:10]:
+            print search.get_filename(cand[1])
+
+    if feature_active('harris'):
+        print 'Loading Harris vocabulary ...'
+        fname = args.prefix + base + '_harris_vocabulary.pkl'
+        with open(fname, 'rb') as f:
+            sift_vocabulary = pickle.load(f)
+
+
