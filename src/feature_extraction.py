edc34c398ce9e89cd05ab4bd0c03b93421ebe5d7
VM MMA Student <boon.kees@gmail.com>
2014-10-23 16:08:59 +0200

    feature extraction code with actual feature extraction commented out

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*-
 import cv2
 import numpy as np
 import progressbar
 import harris
-import metadata_extraction as meta
+import exifread
+import pyexiv2
 
 def extract_metadata(im_list):
 	features = {}
 	for im_name in im_list:
-		tags = meta.extract_tags(im_name)
-		geotags = meta.extract_exif(im_name)
+		tags = extract_tags(im_name)
+		geotags = extract_exif(im_name)
 		features[im_name] = (tags, geotags)
 		
 	return features	
@@ -23,11 +25,14 @@ def get_harris_features(im_list):
     count = 0
     for im_name in im_list:
         im = cv2.imread(im_name, 0)
-        #response = harris.compute_harris_response(im)
-        response = cv2.cornerHarris(im, 7, 5, 0.05)
-        points = harris.get_harris_points(response)
-        desc = harris.get_descriptors(im, points)
-        features[im_name] = np.array(desc)
+        # Exercise 2.4
+        # add code for extracting Harris features from im here
+        # strore them in the features dict keyed by im_name
+        #TODO REMOVE: response = harris.compute_harris_response(im)
+        #TODO REMOVE: response = cv2.cornerHarris(im, 7, 5, 0.05)
+        #TODO REMOVE: points = harris.get_harris_points(response)
+        #TODO REMOVE: desc = harris.get_descriptors(im, points)
+        #TODO REMOVE: features[im_name] = np.array(desc)
         bar.update(count)
         count += 1
     bar.finish()
@@ -48,9 +53,15 @@ def get_colorhist(im_list):
         color_hist = np.zeros((256,3))
         chan_nbr = 0
         for chan in chans:
-            color_hist[:,chan_nbr] = cv2.calcHist([chan], [0], None, [256], [0, 256])[0] / (chan.shape[0] * chan.shape[1]);
+            # Exercise 2.5
+            # Add code to calculate the histogram for each color channel chan here
+            # Store each histogram in color_hist[:,chan_nbr] 
+            # Each histrogram should have 256 bins.
+            # Don't forget to store the color histogram in the features dict keyed
+            # by im_name again, once you've looped through the 3 color channels.
+            # TODO REMOVE: color_hist[:,chan_nbr] = cv2.calcHist([chan], [0], None, [256], [0, 256])[0] / (chan.shape[0] * chan.shape[1]);
             chan_nbr += 1
-        features[im_name] = color_hist
+        # TODO REMOVE: features[im_name] = color_hist
         bar.update(count)
         count += 1
     bar.finish()
@@ -69,8 +80,59 @@ def get_sift_features(im_list):
     for im_name in im_list:
         bar.update(count)
         im = cv2.imread(im_name, 0)
-        kp, desc = sift.detectAndCompute(im, None)
-        features[im_name] = desc
+        # Exercise 2.3
+        # add code for extracting SIFT features from im here.
+        # store them in the features dict keyed by im_name
+        
+        # TODO REMOVE: kp, desc = sift.detectAndCompute(im, None)
+        # TODO REMOVE: features[im_name] = desc
         count += 1
     bar.finish()
     return features
+    
+# extract tags
+def extract_tags(filename):
+    try:
+        exif_data = pyexiv2.ImageMetadata(filename)
+        exif_data.read()
+        tags = exif_data['Exif.Photo.UserComment'].value.split(',')
+        tags = [t.strip() for t in tags]
+        return tags
+    except:
+        print 'No tags could be found for: ' + filename
+        return []
+
+# extract exif
+def extract_exif(filename):
+    # find if there is a geolocation tag, and if so return it.
+    # if geolocation was not turned on, return 'no-geotag'
+    
+    with open(filename) as f:
+        exif_tags = exifread.process_file(f)
+        if 'GPS GPSLongitude' in exif_tags:
+            # assume that all other GPS tags are in there now
+            '''
+            GPS GPSLongitude [4, 21, 761/20]
+            GPS GPSImgDirection 17621/362
+            GPS GPSLatitude [52, 0, 1054/25]
+            GPS GPSDate 2014:02:18
+            Image GPSInfo 704
+            GPS GPSLatitudeRef N
+            GPS GPSImgDirectionRef T
+            GPS GPSAltitudeRef 0
+            GPS GPSTimeStamp [11, 56, 6]
+            GPS GPSAltitude 3653/1134
+            GPS GPSLongitudeRef E
+            '''
+            longitude     = [x.num / x.den for x in exif_tags['GPS GPSLongitude'].values]
+            latitude     = [x.num / x.den for x in exif_tags['GPS GPSLatitude'].values]
+            longRef        = exif_tags['GPS GPSLongitudeRef'].values
+            latRef        = exif_tags['GPS GPSLatitudeRef'].values        
+            
+            friendly_name = str(longitude[0]) + 'd ' + str(longitude[1]) +'\' ' + str(longitude[2]) +'\'\' ' + longRef 
+            friendly_name += ', ' + str(latitude[0]) + 'd ' + str(latitude[1]) +'\' ' + str(latitude[2]) +'\'\' ' + latRef 
+            
+            return (longitude, longRef, latitude, latRef, friendly_name)
+            
+        
+    return 0
--- src/metadata_distance.py
+++ src/metadata_distance.py
@@ -8,127 +8,31 @@ from geopy.distance import vincenty
 
 
 def has_geotag(d):
-	# geotag is at position 1, has zero value if geotag was not found included
-	return d[1] != 0
+    # geotag is at position 1, has zero value if geotag was not found included
+    return d[1] != 0
 
 def lonlat_to_decimal(geo):
-	# example ([52, 0, 36], 'N', [4, 21, 36], 'E', 'delft')
-	result = [geo[0][0] + geo[0][1] / 60.0 + geo[0][2]/3600.0, geo[2][0] + geo[2][1] / 60.0 + geo[2][2]/3600.0]	
-	
-	if geo[1] == 'S':
-		result[0] = -result[0]
-	if geo[3] == 'W':
-		result[1] = -result[1]
-		
-	return (result[0], result[1])	
-		
+    # example ([52, 0, 36], 'N', [4, 21, 36], 'E', 'delft')
+    result = [geo[0][0] + geo[0][1] / 60.0 + geo[0][2]/3600.0, geo[2][0] + geo[2][1] / 60.0 + geo[2][2]/3600.0]    
+    
+    if geo[1] == 'S':
+        result[0] = -result[0]
+    if geo[3] == 'W':
+        result[1] = -result[1]
+        
+    return (result[0], result[1])    
+        
 def compute_geographic_distance(m1,m2):
-	geographical_dist = -1
-	if not has_geotag(m1) or not has_geotag(m2):
-		# can't do geographic distance
-		geographical_dist = 0		
-	else:
-		geo1 = lonlat_to_decimal(m1[1])
-		geo2 = lonlat_to_decimal(m2[1])
-		
-		geographical_dist = vincenty(geo1,geo2)
-	return geographical_dist.meters
-
-def compute_predefined_tags_distance(m1,m2):
-	'''
-	format is:
-	vvv_y_xx_z_w_tag.jpg
-	vvv 	name code													[name] 0
-	y		tourist (t), student (r), unknown (y)						[perspective] 1
-	xx		Oude Jan (oj), Nieuwe Kerk (nk), Raadhuis (rh), Other (xx)	[object] 2 
-	z		Positive (p), Negative (n), None (z)						[mood] 3
-	w		Depicts water (w), Doesn't depict water (m)					[water] 4
-	tag		Tag of choice												[tag] 5
-	'''
-	tagsA = m1[0][:4]
-	tagsB = m2[0][:4]
-	distance = 0
-	
-	# if the tags are known and different, add score
-	# if both are 'unknown', do not consider them to be different
-	
-	# we only have three cases, but if we'd had more we might need to make 
-	# the code a bit more generalized	
-	if not (tagsA[0] == tagsB[0] and tagsA[0] != 'y'):
-		distance += 1
-		
-	if not(tagsA[1] == tagsB[1] and tagsA[1] != 'xx'):
-		distance += 1
-		
-	# we consider two None tags as the same
-	if not (tagsA[2] == tagsB[2]):
-		distance += 1
-		
-	if not (tagsA[3] == tagsB[3]):
-		distance += 1	
-		
-	return distance
-	
-	
-def metadata_ranking(list_of_metadata, query_metadata):
-	'''
-	idea: 
-	When is geotagging a good measure for similarity?
-	If two queries are really close together.
-	What does it mean to be 'close'?
-	Limburg and Texel are 'close' compared to Los Angeles and New York.
-	Limburg and Texel are not 'close' compared to Oude Jan and Nieuwe Kerk
-	
-	Use hierarchical clustering of all data on distance to query image
-	Sort the cluster that is closest on distance. Find standard deviation of 
-	the cluster. Define ties as those clusters within one standard deviation 
-	of eachother. Rank the ties based on predefined tag similarities.
-	If we still have ties, rank based on levenstein of title
-	
-	If we still have ties, the differences are probably not worth mentioning.
-	
-	Is this justifiable?
-	'''
-	geotagged_images = []
-	not_geotagged_images = []
-	
-	if has_geotag(query_metadata):
-		geotagged_images.append(query_metadata) # query is the first element
-		for m in list_of_metadata:
-			if has_geotag(m):
-				geotagged_images.append(m)
-			else:
-				not_geotagged_images.append(m)
-	
-	(clusters, clus_avg_distances) 	= rank_by_geocluster(geotagged_images)	
-	top_predef_candidates 			= rank_by_predef_tags(query_metadata, geotagged_images, clusters, clus_avg_distances)
-	final_ranking 					= rank_by_free_tags(query_metadata, geotagged_images, top_predef_candidates)
-	# !!! now WTF to do with the images that do not have a geotag !!!
-	
-	for c in final_ranking:
-		# print rank, friendly name
-		print c[0] , c[1][0][4]	
-	
-	
-	
-
-#delft = ([52, 0, 36], 'N', [4, 21, 36], 'E', 'delft')
-#fiji = ([17, 11, 9.6], 'S', [178, 39, 32], 'E', 'fiji')
-#newyork = ([40, 39, 51], 'N', [73, 56, 19], 'W', 'newyork')
-#moskow = ([55, 45, 0], 'N', [37, 37, 12], 'E', 'moscow')
-#santiago = ([33, 27, 36], 'S', [70,38,24], 'W', 'santiago')
-#lagos = ([6,0,27],'N',[3,28,12],'E','lagos')
-#bergen = ([60,22,48],'N',[5,20,24],'E','bergen')
-
-#foo = [delft, fiji, newyork, moskow, santiago, lagos, bergen]
-#for a in foo:
-	#for b in foo:
-		#if a[4] != b[4]:
-			#print(a[4] + " to " + b[4]+ ": " + str(compute_geographic_distance(a,b)) +"m") 
-
-
-
-
+    geographical_dist = -1
+    if not has_geotag(m1) or not has_geotag(m2):
+        # can't do geographic distance
+        geographical_dist = 0        
+    else:
+        geo1 = lonlat_to_decimal(m1[1])
+        geo2 = lonlat_to_decimal(m2[1])
+        
+        geographical_dist = vincenty(geo1,geo2)
+    return geographical_dist.meters
 
 
 
