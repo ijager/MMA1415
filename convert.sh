ccbdf7d8160c7df244cd2ed9aef94ce9857b9ef5
Kees Boon <boon.kees@gmail.com>
2014-11-06 13:23:20 +0100

    convert script

---
+++ convert.sh
@@ -0,0 +1,11 @@
+for i in *.ogv; do
+    avconv -i "${i%.ogv}.ogv" -acodec aac -strict experimental "${i%.ogv}.mp4"
+done
+
+for i in *.3gp; do
+    avconv -i "${i}" -r 29.97 -vcodec libx264 -acodec aac -strict experimental "${i%.3gp}.mp4"
+done
+
+for i in *.mp4; do
+    avconv -i "${i}" -ar 16000 "${i%.mp4}.wav"
+done
